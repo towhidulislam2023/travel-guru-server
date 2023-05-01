8624fb95cb293311f203c18441e5e21efb4dfb36
TOWHIDul Islam <towhidulislam2.bd@gmail.com>
2023-05-01 13:09:24 +0600

    complite

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # travel-guru-server
 
-server link: https://tarvel-guru-server-towhidulislam2023.vercel.app/
+# server link: https://tarvel-guru-server-towhidulislam2023.vercel.app/
+## live Link:
+ https://travel-guru-a7939.web.app/
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-
+const cors = require("cors")
+app.use(cors())
 const travelDestination=require('./data/travel-destination.json')
+const hotelData=require("./data/hoteldata.json")
 app.get('/', (req, res) => {
     res.send('Travel Guru Running..........')
 })
 app.get('/destination', (req, res) => {
     res.send(travelDestination)
+})
+app.get('/hotel', (req, res) => {
+    res.send(hotelData)
+})
+app.get('/hotel/:id', (req, res) => {
+    const id =req.params.id
+    const selectedHotels=hotelData.filter(onehotelData=>onehotelData.place_id===id)
+    res.send(selectedHotels)
 })
 
 app.listen(port, () => {
